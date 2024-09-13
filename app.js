640195dc7ccd4fc06b145b4f8645b23039567d40
aashaikhdevloper <145169507+aashaikhdevloper@users.noreply.github.com>
2024-09-14 02:14:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// const arr = [
+//     [1]
+//     [91,92]
+//     [4,6,8]
+//     [7,9,31,23]
+//     [91,17,10,11,12,13]
+
+// ];
+// for (let i = 0; i < arr.length; i++) {
+   
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+const arr = [
+    [1],
+    [91, 92],
+    [4, 6, 8],
+    [7, 9, 31, 23],
+    [91, 17, 10, 11, 12, 13]
+];
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
+    }
+}
